adc0a1f5f09d1aa608a975c165067d48f60a473d
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-01-30 23:24:09 -0500

    better validation

--- lib/rubycron.rb
+++ lib/rubycron.rb
@@ -3,14 +3,23 @@ require 'utility_functions'
 
 module Rubycron
   def self.parse(expression)
-    return 'Invalid Format' if expression =~ /[^\*\d\-,\/LW#\s]/
     return 'Invalid Format' if expression.split(/\s+/).size < 5
 
-    minute, hour, day, month, weekday, year = expression.split(/\s+/)
+    @minute, @hour, @day, @month, @weekday, @year = expression.split(/\s+/)
+    return 'Invalid Format' unless valid?
 
-    time = TimeParser.parse(minute, hour)
-    day = DayParser.parse(day, month, weekday)
+    time = TimeParser.parse(@minute, @hour)
+    day = DayParser.parse(@day, @month, @weekday)
     result = day == 'every day' && time !~ /^at \d{2}:\d{2}$/ ? "#{time}" : "#{time} #{day}"
     result.sub(/^(.)/) { $1 == 'x' ? $1 : $1.upcase}
+  end
+
+  def self.valid?
+    @minute =~ /^[\*\/,\-\d]+$/ &&
+    @hour =~ /^[\*\/,\-\d]+$/ &&
+    @day =~ /^[\*\/,\-\?LW\d]+$/ &&
+    @month =~ /^[\*\/,\-\djanfebmrpyjulgsoctv]+$/i &&
+    @weekday =~ /^[\*\/,\-\?\dsunmotewdhfraL#]+$/i &&
+    (@year.nil? || @year  =~ /^[\*\/,\-\d]*$/)
   end
 end
