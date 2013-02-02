2060e0eed41a6aff6a8681d3dc4b6288f64e0c98
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-02-02 14:23:01 -0500

    update some utility functions

--- lib/utility_functions.rb
+++ lib/utility_functions.rb
@@ -1,9 +1,9 @@
 class Array
   def to_sentence
-    if self.size == 2
-      self.join(' and ')
-    else
-      self[0..-2].join(', ') + " and #{self.last}"
+    case self.size
+    when 1 then self.first
+    when 2 then self.join(' and ')
+    else        self[0..-2].join(', ') + " and #{self.last}"
     end
   end
 end
@@ -12,12 +12,12 @@ class String
   def two_digits
     self.rjust(2, '0')
   end
+
+  def ordinal; self.to_i.ordinal; end
 end
 
 class Numeric
-  def two_digits
-    self.to_s.two_digits
-  end
+  def two_digits; self.to_s.two_digits; end
 
   def ordinal
     return "#{self}th" if self.between?(10, 20)
